cf1d8c6b356e87616d6c45bcd4a4d23dde17359d
romkatsu <r@tsurkanu.ru>
2020-08-24 10:09:27 +0300

    fix stub

--- tests/Stub/FakeIdentity.php
+++ tests/Stub/FakeIdentity.php
@@ -8,9 +8,9 @@ use Yiisoft\Auth\IdentityInterface;
 
 class FakeIdentity implements IdentityInterface
 {
-    private string $id;
+    private ?string $id;
 
-    public function __construct(string $id)
+    public function __construct(?string $id)
     {
         $this->id = $id;
     }
