2ea082f2c7dc9fe3e453fc3634cda585bea0db84
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-08-07 10:27:14 +0300

    Replace Composer Require Checker to Composer Dependency Analyser

---
+++ composer-dependency-analyser.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
+
+return (new Configuration())
+    ->disableComposerAutoloadPathScan()
+    ->setFileExtensions(['php'])
+    ->addPathToScan(__DIR__ . '/config', isDev: false)
+    ->addPathToScan(__DIR__ . '/src', isDev: false)
+    ->addPathToScan(__DIR__ . '/tests', isDev: true)
+    ->ignoreErrorsOnPackages(['jetbrains/phpstorm-attributes', 'yiisoft/yii-debug'], [ErrorType::DEV_DEPENDENCY_IN_PROD]);
