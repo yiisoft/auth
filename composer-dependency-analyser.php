991968ed3257c1067933739d3efff26c3d1fae57
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-08-07 10:36:42 +0300

    fix

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -5,11 +5,16 @@ declare(strict_types=1);
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
 use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
-return (new Configuration())
+$config = (new Configuration())
     ->disableComposerAutoloadPathScan()
     ->setFileExtensions(['php'])
     ->addPathToScan(__DIR__ . '/config', isDev: false)
     ->addPathToScan(__DIR__ . '/src', isDev: false)
     ->addPathToScan(__DIR__ . '/tests', isDev: true)
-    ->ignoreErrorsOnPackages(['jetbrains/phpstorm-attributes', 'yiisoft/yii-debug'], [ErrorType::DEV_DEPENDENCY_IN_PROD])
-    ->ignoreUnknownClasses(['SensitiveParameter']);
+    ->ignoreErrorsOnPackages(['jetbrains/phpstorm-attributes', 'yiisoft/yii-debug'], [ErrorType::DEV_DEPENDENCY_IN_PROD]);
+
+if (PHP_VERSION_ID < 80200) {
+    $config->ignoreUnknownClasses(['SensitiveParameter']);
+}
+
+return $config;
